399c75b0f0b01a5f221d74e05efbcee93a70d1eb
B.M <bigman1208000@gmail.com>
2019-05-15 15:34:59 +0430

    Initial Commit

---
+++ go.mod
@@ -0,0 +1,3 @@
+module onefootball
+
+go 1.12
---
+++ main.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"onefootball/business"
+    "fmt"
+    "onefootball/concurrency"
+    "onefootball/model"
+)
+
+func main() {
+    var outPuts []model.Output
+    
+    noOfJobs := 100
+    go concurrency.Allocate(noOfJobs)
+    done := make(chan bool)
+    go concurrency.GetResult(done)
+    concurrency.CreateWorkerPool(&outPuts)
+    <-done
+
+    fmt.Println("-----------------------------\nResult:\n")
+    business.Print(outPuts)
+
+}
